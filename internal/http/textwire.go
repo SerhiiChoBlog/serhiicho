a8465bd538da9e838b55bbd43e8035febd757a81
SerhiiCho <serhiicho@protonmail.com>
2026-04-19 08:25:19 +0300

    feat: add custom error page

--- internal/http/textwire.go
+++ internal/http/textwire.go
@@ -15,8 +15,9 @@ func newTpl() *textwire.Template {
 	isDev := os.Getenv("APP_ENV") == "development"
 
 	twConf := &config.Config{
-		DebugMode:   isDev,
-		FileWatcher: isDev,
+		DebugMode:     isDev,
+		FileWatcher:   isDev,
+		ErrorPagePath: "error-page",
 		GlobalData: map[string]any{
 			"env":             os.Getenv("APP_ENV"),
 			"appName":         os.Getenv("APP_NAME"),
